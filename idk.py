3c9824d82df92a1ffa172dd344dc90af38f95b77
bobbi g <39740428+glen77777@users.noreply.github.com>
2020-06-23 15:30:12 -0400

    Update idk.py

--- idk.py
+++ idk.py
@@ -10,7 +10,7 @@ comp = float(vol / cr)
 print("volume=", vol, "liter(s)","compressed volume =", comp) 
 while (psi <= 35):
 	eff = float(cr * (1 + (psi / 14.7)))
-        eff2 = float(cr * (vol * (1 + (psi / 14.7))))
-        eff3 = float(cr * psi)
-        print(eff, "    ", eff2, "    ", eff3)
+	eff2 = float(cr * (vol * (1 + (psi / 14.7))))
+	eff3 = float(cr * psi)
+	print(eff, "    ", eff2, "    ", eff3)
 	psi += 1
